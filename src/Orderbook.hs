e06e537640725c23e828b9f73bed4b98b07782b1
aimee-lowth <aimee.lowth@gmail.com>
2013-04-05 18:23:09 +1200

    Update Orderbook.hs
    added comments for rough plan outline

--- src/Orderbook.hs
+++ src/Orderbook.hs
@@ -2,3 +2,10 @@
 module Orderbook (processOrderbook) where
 
 processOrderbook = undefined
+
+-- where do we get the orders from?
+-- assuming orders come in from somewhere in lists of data types (bid and ask)
+-- do we assume they are for one company only?
+-- order asks from lowest to highest (i think)
+-- order bids from highest to lowest (i think)
+-- going to do more research into the process before i write more crap here
