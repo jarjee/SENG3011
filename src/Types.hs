759b89e6dafbb08e9fb435ada38ec09a1ecb1090
Nathan Smyth <z3377335@unsw.edu.au>
2013-04-09 17:17:30 +1000

    Added parser monad
    We've managed to make the code nicer with the tradeoff to those who didn't write it (IE Max and I). Uses the Cassava package.
    
    You can install it via: sudo cabal install cassava

--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE CPP, OverloadedStrings #-}
+
 module Types (getTrades, csvRep) where
+-- THIS REQUIRES CASSAVA FOR ALL THE SPECIAL FUNCTIONALITY, BY DEFAULT IT IS NOT AN INTELLIGENT PARSER.
+
+--BUILD ALL THE CASS WITH -DCASS
+#ifdef CASS
+
+import Control.Applicative
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString as B
+import Data.Csv
+import qualified Data.Vector as V
+import qualified Data.HashMap.Lazy as HM
+
+#endif
+
 -- types go here
 data OrderBookEntry = 
         OrderBookEntry { instrument :: String,
@@ -11,15 +27,58 @@ data OrderBookEntry =
                          value :: Float,
                          qualifiers :: String,
                          transId :: Integer,
-                         bidId :: Integer,
-                         askId :: Integer,
+                         bidId :: Maybe Integer,
+                         askId :: Maybe Integer,
 			 bidAsk :: String,
                          entryTime :: String,
-                         oldPrice :: Float,
+                         oldPrice :: Maybe Float,
                          oldVolume :: Integer,
                          buyerBrokerId :: Integer,
                          sellerBrokerId :: Integer
-        }
+        } deriving (Show, Read, Eq)
+
+#ifdef CASS
+(.:?) :: (FromField a) => NamedRecord -> B.ByteString -> Parser (Maybe a)
+obj .:? key = case HM.lookup key obj of
+		Nothing -> pure Nothing
+		
+		Just v -> do
+			if (not $ B.null v) then 
+				Just <$> parseField (v)
+			else
+				pure Nothing
+
+-- maybe (fail "DICKS") (undefined) (maybe Nothing (parseField) $ HM.lookup key obj) 
+instance FromNamedRecord OrderBookEntry where
+    parseNamedRecord r = do
+	ins <- r.: "#Instrument"
+	date <- read <$> r.: "Date"
+	time <- r.: "Time"
+	reco <- read <$> r.: "Record Type" 
+	pric <- read <$> r.: "Price" 
+	volu <- read <$> r.: "Volume" 
+	undi <- read <$> r.: "Undisclosed Volume" 
+	valu <- read <$> r.: "Value" 
+	qual <- r.: "Qualifiers"
+	tran <- read <$> r.: "Trans ID" 
+	bid <- r .:> "Bid ID" 
+	ask <- r.:> "Ask ID" 
+	bidAsk <- r.: "Bid/Ask"
+	entr <- r.: "Entry Time"
+	oldP <- r.:> "Old Price" 
+	oldV <- read <$> r.: "Old Volume" 
+	buye <- read <$> r.: "Buyer Broker ID" 
+	sell <- read <$> r.: "Seller Broker ID" 		
+
+	return $ OrderBookEntry ins date time reco pric volu undi valu qual tran bid ask bidAsk entr oldP oldV buye sell
+	where
+		obj .:> key = do
+			n <- obj .:? key
+			return $ read <$> n
+
+#else
+
+#endif
 
 -- maybe BidList, AskList and OrderBook types...
 data OrderBook = 
