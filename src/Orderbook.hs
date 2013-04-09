5659ccb5cfa0e944548ef47b78a8b84e68888294
aimee-lowth <aimee.lowth@gmail.com>
2013-04-09 15:42:05 +1000

    Complete Order processing function (except pricestep)

--- src/Orderbook.hs
+++ src/Orderbook.hs
@@ -18,49 +18,59 @@ amendOrder = undefined
 
 deleteOrder = undefined
 
+
+------------------------------------------------------------------------------
+-------- PROCESSING OF ORDER BOOK ENTRIES TO FORM A SORTED ORDERBOOK ---------
+------------------------------------------------------------------------------
+
+-- STEP 1: Split list of orders into bids and asks
+
+splitOrders :: [OrderBookEntry] -> ([OrderBookEntry], [OrderBookEntry])
+splitOrders [] = ([], [])
+splitOrders (x : xs)
+                     | (x . bidAsk == "B"    = (x : bid, ask)
+                     | otherwise = (bid, x : ask)
+                   where
+                     ~(bid, ask) = splitList p xs
+
 -------------------------------------------------------------------------------
 
--- Actually process the order book
-processOrderbook = undefined
--- call ordering functions
--- add spread and step data
+-- STEP 2: Sort orders on time and price criteria
 
--------------------------------------------------------------------------------
-sortBids :: BidList -> BidList
-sortBids bids = sortBy (>)(sortBy (compare `on` time) bids)
+sortBidsAsks :: ([OrderBookEntry], [OrderBookEntry]) -> ([OrderBookEntry], [OrderBookEntry])
+sortBidsAsks orders = sortBids (fst orders), 
+                     sortAsks (snd orders)
 
+sortBids :: [OrderBookEntry] -> [OrderBookEntry]
+sortBids bids = sortBy bidOrdering (sortBy (compare `on` time) bids)
 
---------------------------------------------------------------------------------
-sortAsks :: AskList -> AskList
-sortAsks = undefined
-sortAsks asks = sortBy (<) (sortBy (compare `on` time) asks)
+bidOrdering a b      | a > b = GT
+                     | otherwise LT
 
+sortAsks :: [OrderBookEntry] -> [OrderBookEntry]
+sortAsks asks = sortBy askOrdering (sortBy (compare `on` time) asks)
 
---------------------------------------------------------------------------------
-
-createOrderBook :: BidList -> AskList -> OrderBook
-createOrderBook  = undefined
-
---------------------------------------------------------------------------------
-
-calculateSpread :: BidList -> AskList -> Float
-calculateSpread a b = abs ( ( (head a) . price ) 
-                     - ( (head b) . price ) )
+askOrdering a b      | a < b = GT
+                     | otherwise LT
 
 ---------------------------------------------------------------------------------
 
+-- STEP 3: Calculate stats (spread and price step)
+
+calculateSpread :: ([OrderBookEntry], [OrderBookEntry]) -> Float
+calculateSpread orders = abs ( ( (head (fst orders) ) . price ) 
+                     - ( (head (snd orders) ) . price ) )
+
+calculatePriceStep :: ([OrderBookEntry], [OrderBookEntry]) -> Float
+calculatePriceStep = 1
 -- still not 100% sure what this step business is
-calculatePriceStep :: OrderBook -> Float
-calculatePriceStep = undefined
 
 ---------------------------------------------------------------------------------
 
--- do we assume they are for one company only?
+-- STEP 4: Bring it all together into an OrderBook
 
--- price step: minimum diff in price between 2 groups of same price orders
-
--- we need an orderbook type too (maybe not in here though)
--- data OrderBook = OrderBook BidList AskList
--- type BidList = [Order]
--- type AskList = [Order]
--- as in lists of type Order
+processOrderBook :: [OrderBookEntry] -> OrderBook
+processOrderbook orders = OrderBook       (sortBidsAsks (splitOrders orders))
+                                          calculateSpread (sortBidsAsks (splitOrders orders))
+                                          calculatePriceStep (sortBidsAsks (splitOrders orders))
+------------------------------------------------------------------------------------
