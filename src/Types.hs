c2e4c24a42f73e0974ab9f3fa397987168f86c9e
Nathan Smyth <z3377335@unsw.edu.au>
2013-04-18 23:56:01 +1000

    We now have output with Cassava!
    I'm not going to do output for the simple one, it drives me up the wall to do the parsing. Versus a function that does everything for me? The choice is obvious. I had to remove all my packages though and reset to get this to work, but oh well.

--- src/Types.hs
+++ src/Types.hs
@@ -1,13 +1,12 @@
 {-# LANGUAGE CPP, OverloadedStrings #-}
-
 module Types (
 
-	-- read/write etc
-	getTrades, csvRep,
+    -- read/write etc
+    getTrades, csvRep,
 
-	-- types needed for Orderbook
-	OrderBookEntry, OrderBook(OrderBook), TransId(Bid,Ask), TradeLog,
-	time, price, volume, transId, orders, oldPrice, oldVolume, trans
+    -- types needed for Orderbook
+    OrderBookEntry, OrderBook(OrderBook), TransId(Bid,Ask), TradeLog,
+    time, price, volume, transId, orders, oldPrice, oldVolume, trans
 
 ) where
 
@@ -25,18 +24,19 @@ import qualified Data.HashMap.Lazy as HM
 
 #endif
 
--- types go here
-data OrderBook = 
-				OrderBook 	{ 	orders :: ([OrderBookEntry], [OrderBookEntry]),
-								spread :: Float,
-								priceStep :: Float
-							}deriving (Show, Eq)
+--This is used for the printing for the sake of convenience
+import Data.Maybe
 
--- although this seems pointless at this stage we may need more info later							
-data TradeLog = 
-				TradeLog 	{ 	trades :: ([OrderBookEntry], [OrderBookEntry])
-							}deriving (Show, Eq)							
-							
+-- types go here
+data OrderBook = OrderBook { orders :: ([OrderBookEntry], [OrderBookEntry]),
+                            spread :: Float,
+                            priceStep :: Float
+                            } deriving (Show, Eq)
+
+-- although this seems pointless at this stage we may need more info later                            
+data TradeLog = TradeLog { trades :: ([OrderBookEntry], [OrderBookEntry])
+                          } deriving (Show, Eq)                            
+                            
 data OrderBookEntry = 
         OrderBookEntry { instrument :: String,
                          date :: Integer,
@@ -55,11 +55,11 @@ data OrderBookEntry =
                          oldVolume :: Maybe Integer,
                          -- buyerBrokerId :: Maybe Integer, -- When bidAsk is A
                          -- sellerBrokerId :: Maybe Integer -- When bidAsk is B
-			trans :: Maybe TransId
+            trans :: Maybe TransId
         } deriving (Show, Read, Eq)
 
-data TransId = 	Bid { bidId :: Integer, sellerBrokerId :: Integer }
-				| Ask { askId :: Integer, buyerBrokerId :: Integer } deriving (Show, Read, Eq)
+data TransId =     Bid { bidId :: Integer, sellerBrokerId :: Integer }
+                | Ask { askId :: Integer, buyerBrokerId :: Integer } deriving (Show, Read, Eq)
 
 makeTrans :: Bool -> Maybe Integer -> Maybe Integer -> Maybe TransId
 makeTrans True (Just x) (Just y) = Just $ Bid x y
@@ -69,53 +69,57 @@ makeTrans _ _ _ = Nothing
 #ifdef CASS
 (.:?) :: (FromField a) => NamedRecord -> B.ByteString -> Parser (Maybe a)
 obj .:? key = case HM.lookup key obj of
-		Nothing -> pure Nothing
-		
-		Just v -> do
-			if (not $ B.null v) then 
-				Just <$> parseField (v)
-			else
-				pure Nothing
+        Nothing -> pure Nothing
+        
+        Just v -> do
+            if (not $ B.null v) then 
+                Just <$> parseField (v)
+            else
+                pure Nothing
 
 readF :: IO (Either String (Header, V.Vector OrderBookEntry))
 readF = do
-	f <- BL.readFile "../test/test.csv"
-	return $ decodeByName f
+    f <- BL.readFile "../test/test.csv"
+    return $ decodeByName f
+
+writeF :: ToNamedRecord a => String -> (Header, V.Vector a) -> IO()
+writeF location (head, elements) = do
+	BL.writeFile location (encodeByName head elements)	
 
 db = do
-	x <- readF
-	either (undefined) (doStuff) x
+    x <- readF
+    either (undefined) (doStuff) x
 
 doStuff (h, xs) = return $ V.toList xs
 
 instance FromNamedRecord OrderBookEntry where
     parseNamedRecord r = do
-	ins <- r.: "#Instrument"
-	date <- read <$> r.: "Date"
-	time <- r.: "Time"
-	reco <- read <$> r.: "Record Type" 
-	pric <- r.:> "Price" 
-	volu <- r.:> "Volume" 
-	undi <- r.:> "Undisclosed Volume" 
-	valu <- r.:> "Value" 
-	qual <- r.: "Qualifiers"
-	tran <- read <$> r.: "Trans ID" 
-	bid <- r .:> "Bid ID" 
-	ask <- r.:> "Ask ID" 
-	bidAsk <- r.: "Bid/Ask" :: Parser String
-	let isb = bidAsk == "B"
-	entr <- r.: "Entry Time"
-	oldP <- r.:> "Old Price" 
-	oldV <- r.:> "Old Volume" 
-	buye <- r.:> "Buyer Broker ID" 
-	sell <- r.:> "Seller Broker ID" 		
-	let tranElem = makeTrans isb (if isb then bid else ask) (if isb then sell else buye)
+    ins <- r.: "#Instrument"
+    date <- read <$> r.: "Date"
+    time <- r.: "Time"
+    reco <- read <$> r.: "Record Type" 
+    pric <- r.:> "Price" 
+    volu <- r.:> "Volume" 
+    undi <- r.:> "Undisclosed Volume" 
+    valu <- r.:> "Value" 
+    qual <- r.: "Qualifiers"
+    tran <- read <$> r.: "Trans ID" 
+    bid <- r .:> "Bid ID" 
+    ask <- r.:> "Ask ID" 
+    bidAsk <- r.: "Bid/Ask" :: Parser String
+    let isb = bidAsk == "B"
+    entr <- r.: "Entry Time"
+    oldP <- r.:> "Old Price" 
+    oldV <- r.:> "Old Volume" 
+    buye <- r.:> "Buyer Broker ID" 
+    sell <- r.:> "Seller Broker ID"         
+    let tranElem = makeTrans isb (if isb then bid else ask) (if isb then sell else buye)
 
-	return $ OrderBookEntry ins date time reco pric volu undi valu qual tran entr oldP oldV tranElem
-	where
-		obj .:> key = do
-			n <- obj .:? key
-			return $ read <$> n
+    return $ OrderBookEntry ins date time reco pric volu undi valu qual tran entr oldP oldV tranElem
+    where
+        obj .:> key = do
+            n <- obj .:? key
+            return $ read <$> n
 
 #else
 
@@ -136,17 +140,18 @@ dropPred pr (x:xs) = if pr x then xs else x:dropPred pr xs
 
 getTrades :: String -> [OrderBookEntry]
 getTrades handle = do
-	let records = lines handle
-	map (orderEntry . split (==',')) records
+    let records = lines handle
+    map (orderEntry . split (==',')) records
 
 orderEntry :: [String] -> OrderBookEntry
 orderEntry (inst:dat:tim:recTyp:pri:vol:undisVol:val:qual:trId:bId:aId:ba:entryTim:oldPri:oldVol:buyerBrokId:sellerBrokId:[]) = OrderBookEntry inst (read dat) tim (read recTyp) (read pri) (read vol) (read undisVol) (read val) qual (read trId) entryTim (read oldPri) (read oldVol) transElem
-	where
-		isb = ba == "B"
-		transElem = makeTrans isb (if isb then return $ read bId else return $ read aId) (if isb then return $ read sellerBrokId else return $ read buyerBrokId)
+    where
+        isb = ba == "B"
+        transElem = makeTrans isb (if isb then return $ read bId else return $ read aId) (if isb then return $ read sellerBrokId else return $ read buyerBrokId)
 orderEntry [] = error "orderEntry cannot take in an empty list! CSV file is not of a valid format!"
 orderEntry _ = error "orderEntry must take in exactly the right number of elements! CSV is invalid!"
 
 --Final Output
 csvRep :: [OrderBookEntry] -> String
-csvRep orders = undefined
+csvRep entries = undefined
+
