9a6a4560b57bc28c1e42709d96456f59faf89d44
eckleby <sgpo028@cse.unsw.edu.au>
2013-04-19 17:28:58 +1000

    fixed some errors in Trader.hs
    TradeStats now has some Maybe types to get through the errors.
    updateBidList and updateAskList was using obook as an OrderBookEntry rather than input of OrderBook.
    Still some errors to go

--- src/Trader.hs
+++ src/Trader.hs
@@ -3,6 +3,8 @@ module Trader (generateTrades, Strategy(..)) where
 
 import Types
 import Orderbook
+import Data.Functor
+import Control.Applicative
 
 -----------------------------------------------------------------------------
 ------------------- IMPORTANT NOTES FOR THIS MODULE -------------------------
@@ -22,8 +24,8 @@ import Orderbook
 
 data TradeStats =
 				TradeStats { 	trade :: (OrderBookEntry, OrderBookEntry),
-								tradePrice :: Float,
-								tradeVolume :: Integer,
+								tradePrice :: Maybe Float,
+								tradeVolume :: Maybe Integer,
 								bidID :: Integer,
 								askID :: Integer
 							} deriving (Show, Eq)
@@ -68,10 +70,11 @@ generateTrades orderbook = makeTrade (recordTradeStats orderbook) orderbook
 makeTrade tstats obook = updateBidList tstats (updateAskList tstats obook)
 
 --updateBidList :: TradeStats -> OrderBook -> OrderBook
-updateBidList tstats obook = (head (fst (orders obook))) {volume = (volume - (tradeVolume tstats))}
+updateBidList :: TradeStats -> OrderBook -> OrderBook
+updateBidList tstats obook = obook {orders = ([(head (fst (orders obook))) {volume = ((volume (head (fst (orders obook)))) - (tradeVolume tstats))}] ++ (drop 1 (fst (orders obook))), (snd (orders obook)))}
 
 --updateAskList :: TradeStats -> OrderBook -> OrderBook
-updateAskList tstats obook = (head (snd (orders obook))) {volume = (volume - (tradeVolume tstats))}
+updateAskList tstats obook = obook {orders = ([(head (snd (orders obook))) {volume = ((volume (head (snd (orders obook)))) - (tradeVolume tstats))}] ++ (drop 1 (snd (orders obook))), (snd (orders obook)))}
 
 ------------------------------STEP 3: DELETE USED ORDERS------------------------------------
 				
@@ -79,7 +82,7 @@ updateAskList tstats obook = (head (snd (orders obook))) {volume = (volume - (tr
 pergeOrderBook obook = (pergeOrders (fst (orders obook)), pergeOrders (snd (orders obook)))
 
 --pergeOrders :: [OrderBookEntry] -> [OrderBookEntry]
-pergeOrders os = filter (\o -> (volume o) == 0) os
+pergeOrders os = filter (\o -> (volume o) /= 0) os
 				
 ------------------------------STEP : DETERMINE NEW SPREAD------------------------------------
 				
