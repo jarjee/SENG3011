fad26e99a157893075d46a9c4f7568d32115990c
Nathan Smyth <z3377335@unsw.edu.au>
2013-05-03 17:00:07 +1000

    Started developing traderBrain
    This is going to be extremely useful as it acts
    as the main part of the program loop.

--- src/terribadTrader.hs
+++ src/terribadTrader.hs
@@ -1,3 +1,5 @@
+import Types
+
 --Takes in raw list of OrderBookEntry, filters so we only have ENTER entries
 --Don't think we need to check for only unique entries (this is n^2, so I'd rather not)
 traderEntry :: [OrderBookEntry] -> [OrderBookEntry]
@@ -7,8 +9,14 @@ traderEntry list = filter ((== ENTER) . recordType) list
 --The concept of momentum is created from this (as the first entry comes earlier than the second)
 --Potentially use time as the Y-coordinate?
 difference :: OrderBookEntry -> OrderBookEntry -> Float
+difference = undefined
 
 --The function that runs a running tally of the momentum, money available and shares held.
 --This function is recursive and returns the result at the end.
-traderBrain :: [OrderBookEntry] -> (Float, Float) -> Float -> [OrderBookEntry] -> [OrderBookEntry]
-traderBrain records (bidMomentum, askMomentum) money shares = 
+traderBrain :: [OrderBookEntry] -> Float -> Float -> [OrderBookEntry] -> ([OrderBookEntry], Float)
+traderBrain [] _ money shares = (shares, money)
+traderBrain records momentum money shares = do
+    let resolution = take 10 records
+        remainder = drop 10 records
+        newMomentum = momentum resolution
+    
