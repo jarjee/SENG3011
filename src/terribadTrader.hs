9bc9122dfa8eaa7d5c0813c77f6aa7789194bcb2
Nathan Smyth <z3377335@unsw.edu.au>
2013-05-03 15:11:21 +1000

    Added more sparse functions for the terribadTrader
    We have a whole bunch more functions that need to be implemented
    before we can submit for today.
    
    Hopefully we will be able to demonstrate our ability to process data.

--- src/terribadTrader.hs
+++ src/terribadTrader.hs
@@ -1,6 +1,15 @@
 momentum :: [OrderBookEntry] -> Integer -> numShares -> money -> ([OrderBookEntry], numShares, money)
 
 --Takes in raw list of OrderBookEntry, filters so we only have ENTER entries
+--Don't think we need to check for only unique entries (this is n^2, so I'd rather not)
 traderEntry :: [OrderBookEntry] -> [OrderBookEntry]
 
---Having a recursive tally of momentum, 
+--Take two entries (both bids or both asks) and work out the difference
+--The concept of momentum is created from this (as the first entry comes earlier than the second)
+--Potentially use time as the Y-coordinate?
+difference :: OrderBookEntry -> OrderBookEntry -> Float
+
+--The function that runs a running tally of the momentum, money available and shares held.
+--This function is recursive and returns the result at the end.
+traderBrain :: [OrderBookEntry] -> (Float, Float) -> Float -> [OrderBookEntry] -> [OrderBookEntry]
+traderBrain records (bidMomentum, askMomentum) money shares = 
