83dd8e48072381082b5500295f89611e866f33c0
Nathan Smyth <z3377335@unsw.edu.au>
2013-05-17 19:58:19 +1000

    Added gradientSwitch
    Since the logic is now seperate from the trader, quickcheck
    properties can now be written for this function.

--- src/Trader.hs
+++ src/Trader.hs
@@ -1,31 +1,12 @@
 module Trader (
     traderEntry, TraderState(money,his,sha),
-    defaultTraderState, traderBrain, Share(shAmt,shaPri))
+    defaultTraderState, Share(shAmt,shaPri))--, traderBrain
   where
 
 import Types
 import Data.Maybe
 import Debug.Trace
 import Data.List as L
-
--- |This calculates a given average for a list of OrderBook Entries.
--- We take in a float to allow us to calculate the moving average
--- instead of needing to constantly recalculate from the entire list.
--- The integer is the index of the 'average' that needs to be calculated.
-calcAverage :: [OrderBookEntry] -> Float -> Integer -> Float
-calcAverage oBookEntryList oldAverage index 
-   | index < 0  = oldAverage
-   | index < 10 = (sum' 10 index oBookEntryList) / 10
-   | otherwise  = oldAverage - (fromMaybe 0 (price (oBookEntryList !! (fromIntegral (index - 10)))) / 10) + (fromMaybe 0 (price (oBookEntryList !! (fromIntegral index))) / 10)
-
-sum' :: Integer -> Integer -> [OrderBookEntry] -> Float
-sum' 0 _ list = 0
-sum' _ 0 list = fromMaybe 0 $ price $ (head list)
-sum' amount index list
-   | amount > (index + 1) = sum' (index + 1) index list
-   | amount > (toInteger (length list)) = sum' (toInteger (length list)) index list
-   | index >= (toInteger (length list)) = sum' amount (toInteger (length list - 1)) list
-   | otherwise = (fromMaybe 0 (price (list !! (fromIntegral index)))) + (sum' (amount - 1) (index - 1) list)
 
 {- |Takes in raw list of OrderBookEntry, filters so we only have ENTER entries.
     This is strictly a hack for getting the Trader submission in a useable state.
@@ -50,69 +31,99 @@ data TraderState =
 -- |Initialises a default traderState object so you don't have to.
 defaultTraderState = TraderState [] [] 0 0.0 0.0 [] []
 
+-- Handles the gradient logic for the traderBrain
+gradientSwitch :: [Double] -> TraderState -> (TraderState -> f) -> (TraderState -> f)->(TraderState -> f) -> f
+gradientSwitch entries state peak valley neither = do
+    let first = entries !! 0
+        second = entries !! 1
+        third = entries !! 2
+        gradient = (first - second)
+        secGradient = (second - third)
+    if (gradient/secGradient) <= 0 then do
+        --We've reached a peak or a valley.
+        if (secGradient > 0) then peak state else valley state
+    else
+        neither state
+--Some debug functions for use with the gradient tester
+printPeak :: TraderState -> Int
+printPeak s = 1
+
+printValley :: TraderState -> Int
+printValley s = 1
+
+printNeither :: TraderState -> Int
+printNeither s = 0
+
+--Runs the gradient switch on a set of data and allows us to test the
+--accuracy of our measurements
+gradientTest :: [Double] -> [Int]
+gradientTest l = map (test l) [3..length(l)]
+    where test l x = gradientSwitch (reverse $ take x l) (defaultTraderState) (printPeak) (printValley) (printNeither)
+
+--Generating the list of values we want to test on
+-- map ((+70). (*10) . sin . (pi*)) [0,0.025..2]
 {- |This function handles the decision tree for given gradients.
     More of the buying/selling logic can be seperated out, which will
     not only improve code legibility but fuffil the requirement of
     having multiple trading strategies within the code.
 -}
-traderBrain :: [OrderBookEntry] -> TraderState -> TraderState
-traderBrain [] result = result
-traderBrain (x:allRecords) current = do
-   let newKnown = (kn current) ++ [x]
-       newLength = (knLength current) + 1
-       momentum = momtm current
-       newAskList = if (isBid (trans x) /= True) then sortBy (priceComp) (x:knAsks current) else knAsks current
-
-       newMomentum = calcAverage newKnown momentum (newLength-1)
-       secgradient = (calcAverage newKnown newMomentum (newLength-2))
-       thrdgradient = (calcAverage newKnown secgradient (newLength-3))
-       gradient = (secgradient - thrdgradient)
-
-       resultState = current {kn = newKnown, knLength = newLength, momtm = newMomentum}
-       shares = sha current
-       histo = his current
-       currMoney = money current
-
-   if abs((newMomentum - momentum)/momentum) <= epsilon then
-        --We've reached a peak/valley. Buy or sell accordingly.
-
-        --Sell everything we're worth. Assume we always succeed.
-        if gradient > 0.002 then do
-            let sellUpdate = resultState {
-                money = (currMoney+shareVal gradient (shares)), 
-                sha = [], his = histo ++ shares}
-            traderBrain allRecords sellUpdate
-
-        --Buy as many shares as we can. Ideally from the cheapest source.
-        else if gradient <= 0 then do
-            let result = buyShares' newAskList currMoney
-                updatedShares = if shAmt (bouSha result) /= 0 then resultState {sha = (bouSha result):shares, kn = remShares result, knLength = toInteger $ length $ remShares result} else resultState
-
-            --knLength must be -1 as we remove a record when we buy
-            traderBrain allRecords $ updatedShares { 
-            money = (remMoney result)}
-
-        --Otherwise just don't bother.
-        else traderBrain allRecords $ resultState 
-   else
-        --Simply continue. We haven't reached anything noteworthy.
-        traderBrain allRecords $ resultState 
-    where        
-        shareVal gradient orders = (sum $ map (\x -> (0.0005 + shaPri x) * fromInteger (shAmt x)) orders)
+--traderBrain :: OrderBookEntry -> TraderState -> TraderState
+--traderBrain x current = do
+--   let newKnown = (kn current) ++ [x]
+--       newLength = (knLength current) + 1
+--       momentum = momtm current
+--       newAskList = if (isBid (trans x) /= True) then sortBy (priceComp) (x:knAsks current) else knAsks current
+--
+--       newMomentum = calcAverage newKnown momentum (newLength-1)
+--       secgradient = (calcAverage newKnown newMomentum (newLength-2))
+--       thrdgradient = (calcAverage newKnown secgradient (newLength-3))
+--       gradient = (secgradient - thrdgradient)
+--
+--       resultState = current {kn = newKnown, knLength = newLength, momtm = newMomentum}
+--       shares = sha current
+--       histo = his current
+--       currMoney = money current
+--
+--   if abs((newMomentum - momentum)/momentum) <= epsilon then
+--        --We've reached a peak/valley. Buy or sell accordingly.
+--
+--        --Sell everything we're worth. Assume we always succeed.
+--        if gradient > 0.002 then do
+--            let sellUpdate = resultState {
+--                money = (currMoney+shareVal gradient (shares)), 
+--                sha = [], his = histo ++ shares}
+--            traderBrain allRecords sellUpdate
+--
+--        --Buy as many shares as we can. Ideally from the cheapest source.
+--        else if gradient <= 0 then do
+--            let result = buyShares' newAskList currMoney
+--                updatedShares = if shAmt (bouSha result) /= 0 then resultState {sha = (bouSha result):shares, kn = remShares result, knLength = toInteger $ length $ remShares result} else resultState
+--
+--            --knLength must be -1 as we remove a record when we buy
+--            traderBrain allRecords $ updatedShares { 
+--            money = (remMoney result)}
+--
+--        --Otherwise just don't bother.
+--        else traderBrain allRecords $ resultState 
+--   else
+--        --Simply continue. We haven't reached anything noteworthy.
+--        traderBrain allRecords $ resultState 
+--    where        
+--        shareVal gradient orders = (sum $ map (\x -> (0.0005 + shaPri x) * fromInteger (shAmt x)) orders)
 
 data BoughtShares = BoughtShares { remShares :: [OrderBookEntry],
-                remMoney :: Float,
+                remMoney :: Double,
                 bouSha :: Share} deriving (Show, Eq)
 
 data Share = Share { shAmt :: Integer,
-        shaPri :: Float } deriving (Show, Read, Eq)
+        shaPri :: Double } deriving (Show, Read, Eq)
 
 {-| This is a simple buying strategy that takes in a list of Asks and attempts buy as many items as possible.
     It assumes that it can buy as many records as it wants and in its current form does not generate trade signals.
     It also removes records once it finds a suitable match. The final version of this function will need to update
     the entries according to the volume bought.
 -}
-buyShares' :: [OrderBookEntry] -> Float -> BoughtShares
+buyShares' :: [OrderBookEntry] -> Double -> BoughtShares
 buyShares' [] money = BoughtShares [] money $ Share 0 0
 buyShares' list money = do
     if list /= [] then do
