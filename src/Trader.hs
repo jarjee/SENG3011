d9dd3311e6ce8b53e21047ae1b76486d8b2f5ea2
Nathan Smyth <z3377335@unsw.edu.au>
2013-05-25 21:08:27 +1000

    Implemented the historical decider
    Now all deciders have been implemented. They will all need quickCheck
    properties to make sure that they work correctly, but the main focus
    will be implementing the rest of the deciders.

--- src/Trader.hs
+++ src/Trader.hs
@@ -10,6 +10,7 @@ import Debug.Trace
 import Data.List as L
 import System.IO.Unsafe
 import System.Random
+import Data.Number.Transfinite
 
 {- |Takes in raw list of OrderBookEntry, filters so we only have ENTER entries.
     This is strictly a hack for getting the Trader submission in a useable state.
@@ -29,15 +30,18 @@ data TraderState =
                    money :: Float,
                    his :: [Share],
                    sha :: [Share],
-                   avg :: [Double]} 
+                   avg :: [Double],
+                   hisVal :: (Double, Double)} --left is lowest, right is highest
                    deriving (Show, Eq)
 
 -- |Initialises a default traderState object so you don't have to.
-defaultTraderState = TraderState [] [] 0 0.0 0.0 [] [] []
+defaultTraderState = TraderState [] [] 0 0.0 0.0 [] [] [] (infinity, negativeInfinity)
 
--- Handles the gradient logic for the traderBrain
+-------------------------------
+----------- DECIDERS ----------
+-------------------------------
 gradientSwitch :: (TraderState -> f) -> (TraderState -> f) ->(TraderState -> f) -> TraderState -> f
-gradientSwitch peak valley neither state = do
+gradientSwitch peak valley neither state = if (length $ avg state) < 3 then neither state else do
     let entries = avg state
         first = entries !! 0
         second = entries !! 1
@@ -50,13 +54,26 @@ gradientSwitch peak valley neither state = do
     else
         neither state
 
-nothing :: TraderState -> TraderState
-nothing state = state
-
 randomSwitch :: Float -> Float -> (TraderState -> f) -> (TraderState -> f) -> (TraderState -> f) -> TraderState -> f
 randomSwitch sellProb buyProb sell buy neither state = do
     let x = unsafePerformIO $ randomRIO (0.0, 1.0)
     if x <= sellProb then sell state else if (x > sellProb) && (x < sellProb+buyProb) then buy state else neither state
+
+historicSwitch :: (TraderState -> f) -> (TraderState -> f) -> (TraderState -> f) -> TraderState -> f
+historicSwitch sell buy neither state = do
+    let entries = avg state
+        recorded = hisVal state
+        current = entries !! 0 
+    if current < fst recorded then buy state {hisVal = (current, snd recorded)} 
+    else if current > snd recorded then sell state {hisVal = (fst recorded, current)}
+    else neither state
+
+--------------------------------
+-------- ACTORS ----------------
+--------------------------------
+
+nothing :: TraderState -> TraderState
+nothing state = state
 
 data BoughtShares = BoughtShares { remShares :: [OrderBookEntry],
                 remMoney :: Double,
@@ -90,3 +107,4 @@ priceComp fst snd
      | (maybe (99999) (id) (price fst))  < (maybe (99999) (id) (price snd)) = LT
      | (maybe (99999) (id) (price fst)) == (maybe (99999) (id) (price snd)) = EQ
      | (maybe (99999) (id) (price fst))  > (maybe (99999) (id) (price snd)) = GT
+
